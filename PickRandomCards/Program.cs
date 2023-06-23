a417912aa3c6364676e34718bd963538379b2c23
Șulic Octavian <octavius1992@gmail.com>
2023-06-23 07:43:46 +0300

    Use main method to ask for input and display the cards.

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PickRandomCards;
+
+Console.Write("Enter how many cards you would like to be randomly chosen: ");
+
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int value))
+{
+    string[] cards = CardPicker.PickSomeCards(value);
+    string message = "Your chosen cards are:";
+    foreach(string card in cards) message += $" {card},";
+    message = message.Substring(0, message.Length - 1) + '.';
+    Console.WriteLine(message);
+}
+else
+{
+    Console.Write("The input is not a valid number! Please try again: ");
+}
